a0498d3c4c0683d988a95aaec8b4baa31c4e5abd
Deepak Agrawal <deepakagrawal9011@gmail.com>
2023-04-05 13:11:03 +0530

    new1

--- hello.php
+++ hello.php
@@ -13,8 +13,16 @@ for($a = 0; $a <= $arrlen; $a++) {
 echo $fruit[$a];
 echo "<br>";
 }
-?>
 
+$arr = array("ram"=>"2", "rohan"=>"5", "rajat"=>"8");
+
+foreach($arr as $a => $value){
+
+echo $a." "."has experience of"." " .$value." "."years";
+echo "<br>";
+}
+
+?>
 </body>
 </html>
-?>
+

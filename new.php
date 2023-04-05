d1aa2d29e6167775249b628d004885d222179e8c
Deepak Agrawal <deepakagrawal9011@gmail.com>
2023-04-05 13:12:01 +0530

    new1

--- new.php
+++ new.php
@@ -3,7 +3,7 @@ g$arr = array("ram"=>"2", "rohan"=>"5", "rajat"=>"8");
 
 foreach($arr as $a => $value){
 
-echo $a." "."has experience of"." " .$value." "."years";
+echo $a. "  has experience of  " .$value." years.";
 echo "<br>";
 }
 ?>

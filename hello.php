e6fe6d32f1f3250961725b839ccb1922117624d1
Deepak Agrawal <deepakagrawal9011@gmail.com>
2023-04-05 12:06:39 +0530

    second

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+echo "new";
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$fruit = array("mango","apple","banana","custardapple");
+$arrlen = count($fruit);
+
+for($a = 0; $a <= $arrlen; $a++) {
+echo $fruit[$a];
+echo "<br>";
+}
+?>
+
+</body>
+</html>
+?>

c3ee0539d045f74eef85d5a5d23c21f86886a736
Deepak Agrawal <deepakagrawal9011@gmail.com>
2023-04-07 16:24:52 +0530

    Q1

--- new.php
+++ new.php
@@ -138,6 +138,16 @@ echo current($stdt);
      echo "<br>";
     
 
+     $ceu = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+
+asort($ceu);
+foreach($ceu as $key => $value)
+{
+echo "The capital of $key is $value.";
+echo "<br>";
+}
+
+
 
     
    

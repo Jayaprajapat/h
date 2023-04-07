cd6b4badd46d4529af81b78c87ccd631cb665a11
Deepak Agrawal <deepakagrawal9011@gmail.com>
2023-04-07 17:14:04 +0530

    sort

--- new.php
+++ new.php
@@ -150,7 +150,21 @@ echo "<br>";
 $color = array(4 => 'white', 6 => 'green', 11=> 'red');
  print_r(reset($color));
  echo "<br>"; 
- 
+
+ $arr = array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+ asort($arr);
+ print_r($arr);
+ echo "<br>";
+ ksort($arr);
+ print_r($arr);
+ echo "<br>";
+ arsort($arr);
+ print_r($arr);
+ echo "<br>";
+  krsort($arr);
+ print_r($arr);
+ echo "<br>";
+
 }
 
 
